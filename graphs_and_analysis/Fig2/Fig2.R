3bbf920b3b248f93d66164d07fffb14f237077b1
hartman424 <133162442+hartman424@users.noreply.github.com>
2023-06-20 07:37:22 -0400

    Add files via upload

--- graphs_and_analysis/Fig2/Fig2.R
+++ graphs_and_analysis/Fig2/Fig2.R
@@ -5,8 +5,8 @@ library(stringi)
 rm(list = ls())
 
 # Load centrality and switching rate data
-gd <- read.csv("filepath\\gd.csv")
-centrality <- read.csv("filepath\\centrality.csv")
+gd <- read.csv("C:\\Users\\raven\\Documents\\gd.csv")
+centrality <- read.csv("C:\\Users\\raven\\Documents\\centrality.csv")
 
 # Make two lists of switching rate and grooming degree
 switch <- c(centrality$roost_switch,gd$cs,gd$ps2)
@@ -35,54 +35,55 @@ gd2$groomed <- as.numeric(gd2$groomed)
 
 # Graph label
 label <- c(
-  "a" = "(a) Roost Switching (switches / day)",
-  "b" = "(b) Cluster Switching (switches / day)",
-  "c" = "(c) Within-Cluster Partner Switching (switches / hour)"
+  "a" = "Roost Switching (switches / day)",
+  "b" = "Cluster Switching (switches / day)",
+  "c" = "Partner Switching (switches / hour)"
 )
 
 # Graph theme
 theme_new <- theme_set(theme_bw())
-theme_new <- theme_update(strip.text = element_text(hjust = 0), strip.placement = "outside", axis.title.x=element_blank(), strip.background = element_blank())
+theme_new <- theme_update(strip.text = element_text(), strip.placement = "outside", axis.title.x=element_blank(), strip.background = element_blank())
 
 # Plot
 ggplot(data = gd2, mapping =  aes(x = switch, y = groomed)) + 
   geom_point() +
   geom_smooth(method = 'lm') +
   ylab("Partners Groomed")  +
-  facet_wrap(~type, nrow = 3, scales = "free", strip.position = "bottom", labeller = as_labeller(label)) +
+  facet_wrap(~type, nrow = 1, scales = "free", strip.position = "bottom", labeller = as_labeller(label)) +
   geom_smooth(data = data.frame(gd2,z="c"), method = '')
 
 # Run permutation tests and get statistic metrics, relationship metrics
-model <- lm(centrality~roost_switch, data=centrality)
-summary(model)
-root <- rep(0,1000)
-for (i in 1:1000) {
+model <- lm(centrality~roost_switch, data=centrality) # Get the roost switching linear model
+summary(model) # Look at statstics associated with the model
+root <- rep(0,1000) # Initialize vector
+for (i in 1:1000) { # Run 1000 permutations and find the R^2 value
   data2 <- centrality[sample(nrow(centrality),size = nrow(centrality),replace = T),]
   root[i] <- summary(lm(centrality ~ roost_switch, data = data2))$r.squared
 }
-quantile(root, probs = c(0.025,0.975))
-lm(data.frame(scale(model$model)))
-model <- lm(n~cs, data=gd)
-summary(model)
-coot <- rep(0,1000)
-for (i in 1:1000) {
+quantile(root, probs = c(0.025,0.975)) # Find where 95% of the R^2 values lie
+lm(data.frame(scale(model$model))) # And observe more statistics
+model <- lm(n~cs, data=gd) # Get the cluster switching linear model
+summary(model) # Look at statistics
+coot <- rep(0,1000) # Initialize vector
+for (i in 1:1000) { # Run 1000 permutations and find the R^2 value
   data2 <- gd[sample(nrow(gd),size = nrow(gd),replace = T),]
   coot[i] <- summary(lm(n ~ cs, data = data2))$r.squared
 }
-quantile(coot, probs = c(0.025,0.975))
-lm(data.frame(scale(model$model)))
-model <- lm(n~ps, data=gd)
-summary(model)
-lm(data.frame(scale(model$model)))
-model <- lm(n~ps2, data=gd)
-summary(model)
-poot <- rep(0,1000)
-for (i in 1:1000) {
+quantile(coot, probs = c(0.025,0.975)) # Find where 95% of the R^2 values lie
+lm(data.frame(scale(model$model))) # And observe more statistics
+model <- lm(n~ps, data=gd) # Get the partner switching linear model
+summary(model) # Look at statistics
+lm(data.frame(scale(model$model))) # And view more statistics
+model <- lm(n~ps2, data=gd)  # Get the within-cluster partner switching linear model
+summary(model) # View statistics
+poot <- rep(0,1000) # Initialize vector
+for (i in 1:1000) { # # Run 1000 permutations and find the R^2 value
   data2 <- gd[sample(nrow(gd),size = nrow(gd),replace = T),]
   poot[i] <- summary(lm(n ~ ps2, data = data2))$r.squared
 }
-quantile(poot, probs = c(0.025,0.975))
-lm(data.frame(scale(model$model)))
+quantile(poot, probs = c(0.025,0.975)) # Find where 95% of the R^2 values lie
+lm(data.frame(scale(model$model))) # And observe more statistics
+# Get statistics for the relation between cluster switching and partner switching
 model <- lm(cs~ps, data=gd)
 summary(model)
 lm(data.frame(scale(model$model)))
