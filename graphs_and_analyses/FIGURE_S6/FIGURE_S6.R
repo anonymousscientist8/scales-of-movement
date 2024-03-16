a398b36e3cfc0819ef50952a790b359971d5af4e
anonymousscientist8 <133162442+anonymousscientist8@users.noreply.github.com>
2024-03-16 02:07:17 -0500

    Add files via upload

---
+++ graphs_and_analyses/FIGURE_S6/FIGURE_S6.R
@@ -0,0 +1,51 @@
+rm(list = ls())
+
+# load packages
+library(igraph)
+library(tidyverse)
+
+# Import Data
+perms1 <- read.csv("filepath\\exp548.csv")
+colnames(perms1) <- c("X","exp1")
+perms2 <- read.csv("filepath\\exp2400.csv")
+colnames(perms2) <- c("X","exp2")
+perms3 <- read.csv("filepath\\exp2401.csv")
+colnames(perms3) <- c("X","exp3")
+perms4 <- read.csv("filepath\\exp2402.csv")
+colnames(perms4) <- c("X","exp4")
+perms5 <- read.csv("filepath\\exp2403.csv")
+colnames(perms5) <- c("X","exp5")
+obs <- read.csv("filepath\\obs.csv")
+colnames(obs) <- c("cols","value")
+obs$cols <- c("exp1","exp2","exp3","exp4","exp5")
+
+# bind into data frame
+d <- cbind(perms1,perms2,perms3,perms4,perms5)
+d <- d[,-1]
+d <- d[,-2]
+d <- d[,-3]
+d <- d[,-4]
+d <- d[,-5]
+
+# Add label
+label <- c(
+  "exp1" = "(a) original simulation",
+  "exp2" = "(b) removing hierarchically embedded scales of movement (bats stay within one cluster)",
+  "exp3" = "(c) removal of individual variation in partner switching (bats also have same switching propensity)",
+  "exp4" = "(d) removal of partner fidelity due to location (bats also groom a random available partner every minute)",
+  "exp5" = "(e) removal of variation in behavior (bats also groom at same time)"
+)
+
+# Theme
+theme_new <- theme_set(theme_bw())
+theme_new <- theme_update(strip.background = element_blank(),strip.text = element_text(hjust = 0))
+
+# Plot
+(plot <- 
+    ggplot() + 
+    geom_histogram(data = gather(d, cols, value), aes(x = value), binwidth = 0.001, colour = "lightblue", fill = "lightblue") +
+    geom_hline(yintercept = 0, color = "azure2") +
+    geom_vline(data = obs, aes(xintercept = value), color = "black", linetype = "solid") +
+    facet_wrap(.~cols, scales = "fixed", labeller = as_labeller(label), nrow = 5, strip.position = "top") +
+    xlab("social differentiation: expected (blue) vs observed (black)") +
+    ylab("frequency"))
